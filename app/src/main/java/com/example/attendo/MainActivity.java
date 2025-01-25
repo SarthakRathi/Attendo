f7f340c9e7ed2c59db788aa370209d20017bd58f
SarthakRathi <sarthakrathi89@gmail.com>
2025-01-25 15:13:14 +0530

    toolbar changes

--- app/src/main/java/com/example/attendo/MainActivity.java
+++ app/src/main/java/com/example/attendo/MainActivity.java
@@ -3,22 +3,17 @@ package com.example.attendo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signout;
     ImageView menu;
     TextView company_name;
 
@@ -28,19 +23,8 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        signout = findViewById(R.id.signout_btn);
         menu = findViewById(R.id.menu);
         company_name = findViewById(R.id.company_name);
-
-        signout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,24 +37,17 @@ public class MainActivity extends AppCompatActivity {
     private void showPopupMenu(View anchor) {
         PopupMenu popupMenu = new PopupMenu(this, anchor);
         popupMenu.getMenuInflater().inflate(R.menu.menu_dropdown, popupMenu.getMenu());
-
         popupMenu.setOnMenuItemClickListener(item -> {
-            switch (item.getItemId()) {
-
-                case R.id.sign_out:
-                    // Handle Sign Out click
-                    FirebaseAuth.getInstance().signOut();
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return true;
-                default:
-                    return false;
+            int itemId = item.getItemId();
+            if (itemId == R.id.sign_out) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
             }
+            return false;
         });
-
         popupMenu.show();
     }
-
-
 }
